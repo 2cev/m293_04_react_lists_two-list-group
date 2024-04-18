c63f2988b5ebc20842cda54f7294cd0f17be005f
2cev <tucev@hotmail.com>
2024-04-18 11:03:57 +0200

    Lektion Ende

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import './App.css';
 import SpacerHeight from './Components/Layout/SpacerHeight.jsx';
 import SpacerWidth from './Components/Layout/SpacerWidth.jsx';
 
-import ListVariant001 from './Components/Layout/ListElements/ListVariant001.jsx';
 
 // O_o => bootstraped
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+// O_o => var
+const aryMotorbikes = [
+	{brand:'Ducati', 	type: 'Naked', 		model: 'Monster', 			version: 'S4R', 			year: 2003, ps: 113, cc: 996},
+	{brand:'KTM', 		type: 'Naked', 		model: 'Super Duke', 		version: '1290 R', 			year: 2014, ps: 177, cc: 1301},
+	{brand:'Triumph', 	type: 'Naked', 		model: 'Speed Tripple', 	version: '1200 RS', 		year: 2021, ps: 188, cc: 1160},
+	{brand:'Ducati', 	type: 'SuperSport',	model: '998', 				version: 'S Final Edition', year: 2005, ps: 123, cc: 998},
+	{brand:'Yamaha', 	type: 'SuperSport',	model: 'YZF',	 			version: 'R1', 				year: 2021, ps: 162, cc: 998},
+	{brand:'Honda', 	type: 'SuperSport',	model: 'CBR', 				version: '1000 RR', 		year: 2020, ps: 214, cc: 999},
+];
 
 
 function App() {
 	
 	// O_o => liste der farben
-	// const colors = ['red', 'green', 'blue'];
-	const [colors, setColors] = useState(['#red', '#green', '#blue']);
+	const colors = ['red', 'green', 'blue'];
+	// const [colors, setColors] = useState(['#red', '#green', '#blue']);
 
 	/* * /
 	const alertClicked = (msg) => {
@@ -85,7 +93,7 @@ function App() {
 		<div className="App">
 			<div className='Stage'>
 				<div className='StgWrapper'>
-					{/*  */}{/*  */}{/*  */}
+					{/* ===> ==> => Title */}
 
 					<div className='StgTitle'><h1>
 						M293 :: ReactJS
@@ -114,8 +122,8 @@ function App() {
 							<h3>
 								Eine Liste mit den Farben (red, green und blue) soll in einer Anwendung angezeigt werden.
 								<span className='ClrGreen'>Liste von den Farben erstellen.<br />
-									Dierekt im HTML DOM durch die Liste mit der map()-Funktion erstellt.</span>
-									<span className='ClrRed'>unkontrolliert ohne key auf den Einträgen erstellt.</span>
+									Dierekt im HTML DOM die Liste mit der map()-Funktion erstellt.</span>
+									<span className='ClrRed'>unkontrolliert ohne key auf den Listen-Einträgen erstellt.</span>
 							</h3>
 						</div>
 					</div>
@@ -134,8 +142,8 @@ function App() {
 							<h3>
 								Eine Liste mit den Farben (red, green und blue) soll in einer Anwendung angezeigt werden.
 								<span className='ClrGreen'>Liste von den Farben erstellen.<br />
-									Dierekt im HTML DOM durch die Liste mit der map()-Funktion erstellt.</span>
-									<span className='ClrGreen'>kontrolliert mit key auf den Einträgen erstellt.</span>
+									Dierekt im HTML DOM die Liste mit der map()-Funktion erstellt.</span>
+									<span className='ClrGreen'>kontrolliert mit key auf den Listen-Einträgen erstellt.</span>
 							</h3>
 						</div>
 					</div>
@@ -154,8 +162,7 @@ function App() {
 							<h3>
 								Eine Liste mit den Farben (red, green und blue) soll in einer Anwendung angezeigt werden.
 								<span className='ClrGreen'>Liste von den Farben erstellen.<br />
-									Dierekt im HTML DOM durch die Liste mit der map()-Funktion erstellt.</span>
-									<span className='ClrGreen'>kontrolliert mit key auf den Einträgen erstellt.</span>
+									Dierekt im HTML DOM erstellt mit Bootstrap ListGroup.Item.</span>
 							</h3>
 						</div>
 					</div>
@@ -164,20 +171,22 @@ function App() {
 						<ListGroup defaultActiveKey="#green">
 
 							<ListGroup.Item action href="#green" 
-								onClick={onClicked('o_O => onClick(#green)')} 
-								// onClickCapture={onClickCaptured('o_O => onClockCaptured("#green")')}
+								onClick={() => onClicked('o_O => onClick(#green)')} 
+								onClickCapture={() => onClickCaptured('o_O => onClockCaptured("#green")')}
 								>
 								#green
 							</ListGroup.Item><br />
+
 							<ListGroup.Item action href="#red" 
-								onClick={onClicked('o_O => onClick(#red)')} 
-								// onClickCapture={onClickCaptured('o_O => onClockCaptured(#red)')}
+								onClick={() => onClicked('o_O => onClick(#red)')} 
+								onClickCapture={() => onClickCaptured('o_O => onClockCaptured(#red)')}
 								>
 								#red
 							</ListGroup.Item><br />
+
 							<ListGroup.Item action href="#blue"
-								onClick={onClicked('o_O => onClick(#blue)')} 
-								// onClickCapture={onClickCaptured('o_O => onClockCaptured(#blue)')}
+								onClick={() => onClicked('o_O => onClick(#blue)')} 
+								onClickCapture={() => onClickCaptured('o_O => onClockCaptured(#blue)')}
 								>
 								#blue
 							</ListGroup.Item><br />
@@ -192,8 +201,8 @@ function App() {
 							<h3>
 								Eine ListGroup mit den Farben (red, green und blue) soll in einer Anwendung angezeigt werden.
 								<span className='ClrGreen'>Liste von den Farben erstellen.<br />
-									Dierekt im HTML DOM durch die ListGroup mit der map()-Funktion erstellt.</span>
-									<span className='ClrGreen'>kontrolliert mit key auf den Einträgen erstellt.</span>
+									Dierekt im HTML DOM die Liste mit Bootstrap ListGroup und der map()-Funktion erstellt.</span>
+									<span className='ClrYellow'>kontrolliert mit key auf den Listen-Einträgen erstellt.</span>
 							</h3>
 						</div>
 					</div>
@@ -207,7 +216,7 @@ function App() {
 						</ListGroup>
 					</div>
 
-					<SpacerHeight isVisible={true} isVisibleStrong={false} isDoubleSingle={false} isFullFitWidth={true} />
+					{/* <SpacerHeight isVisible={true} isVisibleStrong={false} isDoubleSingle={false} isFullFitWidth={true} />
 					<div className='DivWrapper ClrGrey'>
 						<div className='DivSubTitle'>
 							<h2><span className='ClrGreen'>Aufgabe 1.4</span></h2><br />
@@ -215,7 +224,7 @@ function App() {
 								Eine Liste mit den Farben (red, green und blue) soll in einer Anwendung angezeigt werden.
 								<span className='ClrGreen'>Liste von den Farben erstellen.<br />
 									Dierekt im HTML DOM durch die Liste mit der map()-Funktion erstellt.</span>
-									<span className='ClrGreen'>kontrolliert mit key auf den Einträgen erstellt.</span>
+									<span className='ClrGreen'>kontrolliert mit key auf den Listen-Einträgen erstellt.</span>
 							</h3>
 						</div>
 					</div>
@@ -232,7 +241,7 @@ function App() {
 						<Button class='w-fit h-fit' onClick={moveBikes('-')}>{'<=<==<==='}</Button><br />
 						<Button class='w-fit h-fit' onClick={moveBikes('+1')}>{'=>'}</Button><br />
 						<Button class='w-fit h-fit' onClick={moveBikes('-1')}>{'<='}</Button><br />
-					</div>
+					</div> */}
 
 					{/*  */}{/*  */}{/*  */}
 				</div>
--- src/Components/Layout/ListElements/ListVariant001.jsx
+++
@@ -1,3 +0,0 @@
-<script>
-    import "./ListVariant001.css"
-</script>
